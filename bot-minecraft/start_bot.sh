e34e45946b6dbeee9fcb94a3431fa9755f050d46
Anothai16 <anothai160347@gmail.com>
2026-08-11 20:05:42 +0700

    test2

--- bot-minecraft/start_bot.sh
+++ bot-minecraft/start_bot.sh
@@ -2,15 +2,24 @@
 cd "$(dirname "$0")"
 
 (
-  sleep 3
+  # 1. รอ 5 วินาที ให้หน้า Dialog โหลดขึ้นมาจนสมบูรณ์
+  sleep 5
   echo "/dialog input pass 112233"
+  
   sleep 1
   echo "/dialog click 1"
-  sleep 4
+  
+  # 2. รอ 5 วินาที ให้เซิร์ฟเวอร์ปลดล็อกล็อกอิน และวาร์ปตัวละครเข้าจุด Spawn
+  sleep 5
   echo "/useitem mainhand"
-  sleep 2
-  echo "/inventory 1 click 10"
+  
+  # 3. รอ 3 วินาที ให้ GUI เข็มทิศเด้งขึ้นมา แล้วสั่งจิ้มเลือกสล็อต 10 (บล็อกหญ้า)
+  sleep 3
+  echo "/inventory container click 10 Left"
+  
+  # 4. รอ 3 วินาที แล้ววาร์ปกลับบ้าน
   sleep 3
   echo "/home home"
+  
   cat
-) | ./MinecraftClient K666 - play.amorycraft.com
+) | ./MinecraftClient-20260809-500-linux-x64 K666 - play.amorycraft.com
