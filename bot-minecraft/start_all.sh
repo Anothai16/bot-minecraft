cdc464d78d4134ce4efdd9e4e61969e851a962b0
Anothai16 <anothai160347@gmail.com>
2026-08-11 20:35:57 +0700

    test3

--- bot-minecraft/start_all.sh
+++ bot-minecraft/start_all.sh
@@ -1,34 +1,27 @@
 #!/bin/bash
 cd "$(dirname "$0")"
 
-# ตั้งค่าบีบ Memory ของ .NET (ยังคงไว้เพื่อประหยัด RAM)
-export DOTNET_gcServer=0
-export DOTNET_GCHeapHardLimit=0x4000000
-export DOTNET_GCConserveMemory=9
+# บังคับ .NET ให้คืน RAM ให้ OS ทันทีที่ว่าง และจำกัด Heap size
+export DOTNET_GCHeapHardLimit=0x2800000   # จำกัด GC Heap อยู่ที่ประมาณ ~40MB
+export DOTNET_GCConserveMemory=9          # ตั้งค่าการประหยัด Memory ระดับสูงสุด (1-9)
+export DOTNET_GCLatencyMode=2             # ใช้ LowMemory GC Mode
 
-BOTS=("obs1" "Morgan05" "Domertown" "Nattanon09" "Nanepez")
+BOTS=("obs1" "Morgan05" "Domertown" "Nattanon09" "Nanepez" "Sudlorkayeejai" "Wood_Skel" "sindirt")
 
 for BOT in "${BOTS[@]}"; do
-    echo "--------------------------------------------------"
     echo "กำลังเปิดบอท: $BOT"
-    echo "--------------------------------------------------"
     
     (
       sleep 10
       echo "/dialog input pass 112233"
-      
       sleep 3
       echo "/dialog click 1"
-      
       sleep 10
       echo "/useitem mainhand"
-      
       sleep 1
       echo "/inventory container click 10 Left"
-      
       sleep 8
       echo "/afk"
-      
       cat
     ) | ./MinecraftClient "$BOT" - play.amorycraft.com &
 
