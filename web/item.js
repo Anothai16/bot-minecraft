47cb2db200d1ca5d2981dcaa3126d4b17c410884
Anothai16 <anothai160347@gmail.com>
2026-08-21 17:07:13 +0700

    test3

---
+++ web/item.js
@@ -0,0 +1,112 @@
+window.RAW_ITEMS = [
+  { 
+    "id": 1,
+    "name": "Relic Super Boots", 
+    "category": "equipment", 
+    "desc": "บินในเขตโพรเทคตัวเอง",
+    "customPrice": "30 P / ชิ้น",
+    "inStock": true, 
+    "img": "https://raw.githubusercontent.com/InventivetalentDev/minecraft-assets/1.20.1/assets/minecraft/textures/item/golden_boots.png",
+    "isEnchanted": false
+  },
+  { 
+    "id": 2,
+    "name": "Relic Super Sell Axe", 
+    "category": "equipment", 
+    "desc": "ขายไอเทมทั้งหมดในกล่องทันที",
+    "customPrice": "20 P / ชิ้น",
+    "inStock": true, 
+    "img": "https://raw.githubusercontent.com/InventivetalentDev/minecraft-assets/1.20.1/assets/minecraft/textures/item/netherite_axe.png",
+    "isEnchanted": true
+  },
+  { 
+    "id": 3,
+    "name": "Relic Super Axe", 
+    "category": "equipment", 
+    "desc": "ตัดต้นไม้ทั้งต้นทันที",
+    "customPrice": "20 P / ชิ้น",
+    "inStock": true, 
+    "img": "https://raw.githubusercontent.com/InventivetalentDev/minecraft-assets/1.20.1/assets/minecraft/textures/item/netherite_axe.png",
+    "isEnchanted": true
+  },
+  { 
+    "id": 4,
+    "name": "Relic Super Pickaxe", 
+    "category": "equipment", 
+    "desc": "ขุด 9 บล็อคในครั้งเดียว",
+    "customPrice": "20 P / ชิ้น",
+    "inStock": true, 
+    "img": "https://raw.githubusercontent.com/InventivetalentDev/minecraft-assets/1.20.1/assets/minecraft/textures/item/netherite_pickaxe.png",
+    "isEnchanted": true
+  },
+  { 
+    "id": 5,
+    "name": "Relic Super Shovel", 
+    "category": "equipment", 
+    "desc": "ขุด 9 บล็อคในครั้งเดียว",
+    "customPrice": "15 P / ชิ้น",
+    "inStock": true, 
+    "img": "https://raw.githubusercontent.com/InventivetalentDev/minecraft-assets/1.20.1/assets/minecraft/textures/item/netherite_shovel.png",
+    "isEnchanted": true
+  },
+  { 
+    "id": 6,
+    "name": "Relic Super Chest", 
+    "category": "blocks", 
+    "desc": "กล่องพิเศษสำหรับเก็บไอเทมจำนวนมากขนาด 250k",
+    "customPrice": "120 P / ชิ้น",
+    "inStock": true, 
+    "img": "https://raw.githubusercontent.com/InventivetalentDev/minecraft-assets/1.20.1/assets/minecraft/textures/block/barrel_side.png",
+    "isEnchanted": false
+  },
+  { 
+    "id": 7,
+    "name": "Relic Super Firework Rocket", 
+    "category": "blocks", 
+    "desc": "ใช้งานพลุได้ไม่จำกัดเมื่อใช้งาน Elytra",
+    "customPrice": "5 P / ชิ้น",
+    "inStock": true, 
+    "img": "https://raw.githubusercontent.com/InventivetalentDev/minecraft-assets/1.20.1/assets/minecraft/textures/item/firework_rocket.png",
+    "isEnchanted": false
+  },
+  { 
+    "id": 8,
+    "name": "Relic Super Bucket", 
+    "category": "blocks", 
+    "desc": "ดูดน้ำออกเป็นวงกว้างทันที",
+    "customPrice": "5 P / ชิ้น",
+    "inStock": true, 
+    "img": "https://raw.githubusercontent.com/InventivetalentDev/minecraft-assets/1.20.1/assets/minecraft/textures/item/bucket.png",
+    "isEnchanted": false
+  },
+  { 
+    "id": 9,
+    "name": "Relic Super Bits", 
+    "category": "food", 
+    "desc": "ได้รับ Bit x4 เป็นเวลา 24 ชั่วโมง",
+    "customPrice": "30 P / ชิ้น",
+    "inStock": true, 
+    "img": "data:image/svg+xml;utf8,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 16 16' width='16' height='16' shape-rendering='crispEdges'><rect x='7' y='1' width='2' height='1' fill='%23896746'/><rect x='6' y='2' width='4' height='1' fill='%23222'/><rect x='7' y='2' width='2' height='1' fill='%23a17e5a'/><rect x='6' y='3' width='4' height='1' fill='%23ffffff' opacity='0.3'/><rect x='5' y='4' width='6' height='1' fill='%23ffffff' opacity='0.2'/><rect x='4' y='5' width='8' height='1' fill='%231b263b'/><rect x='3' y='6' width='10' height='7' fill='%231b263b'/><rect x='4' y='13' width='8' height='1' fill='%231b263b'/><rect x='5' y='14' width='6' height='1' fill='%231b263b'/><rect x='4' y='6' width='8' height='7' fill='%232563eb'/><rect x='4' y='7' width='8' height='6' fill='%233b82f6'/><rect x='5' y='8' width='6' height='5' fill='%2360a5fa'/><rect x='5' y='8' width='1' height='4' fill='%23ffffff' opacity='0.7'/><rect x='6' y='9' width='1' height='2' fill='%23ffffff' opacity='0.9'/><rect x='8' y='10' width='2' height='2' fill='%231d4ed8'/><rect x='4' y='5' width='1' height='8' fill='%23ffffff' opacity='0.35'/></svg>",
+    "isEnchanted": false
+  },
+  { 
+    "id": 10,
+    "name": "Coin (คอยน์)", 
+    "category": "misc", 
+    "desc": "เหรียญคอยน์สำหรับซื้อของในเซิร์ฟเวอร์",
+    "customPrice": "10P / 20M<br>100P / 200M",
+    "inStock": true, 
+    "img": "https://raw.githubusercontent.com/InventivetalentDev/minecraft-assets/1.20.1/assets/minecraft/textures/block/sunflower_front.png",
+    "isEnchanted": false
+  },
+    { 
+    "id": 11,
+    "name": "บริการ AFK", 
+    "category": "misc", 
+    "desc": "AFK ยืนฟาร์ม / ยืนรับ Bit ",
+    "customPrice": "10P / วัน<br>70P / 7 วัน<br>270P / 30 วัน <br>ตัวถัดไปลด 50%",
+    "inStock": true, 
+    "img": "https://raw.githubusercontent.com/InventivetalentDev/minecraft-assets/1.20.1/assets/minecraft/textures/item/clock_00.png",
+    "isEnchanted": true
+  }
+];
